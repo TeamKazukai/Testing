75037bab4c67117f0b24f78437e41e761d9a7ce0
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-09-12 14:37:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/azanhelpdesk/DQ-_TOM.git /DQ-_TOM
+  git clone https://github.com/TeamKazukai/Testing.git /Testing 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-_TOM
+  git clone $UPSTREAM_REPO /Testing 
 fi
-cd /DQ-_TOM
+cd /Testing 
 pip3 install -U -r requirements.txt
-echo "Starting DQ-_TOM...."
+echo "Starting AutoMovieBot...."
 python3 bot.py
